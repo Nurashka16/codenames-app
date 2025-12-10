ccb1c93fe158bafb634bcdc70d85822fdf75ed1c
Nura <tilepovanura@gmail>
2025-12-10 18:11:07 +0300

    test commit2

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,28 +3,37 @@ import LoadingGrid from '../components/LoadingGrid';
 import styles from './Home.module.css';
 
 const Home: React.FC = () => {
-  const [showCreateButton, setShowCreateButton] = useState(false);
+    const [showCreateButton, setShowCreateButton] = useState(false);
 
-  // Через 15 секунд показываем кнопку
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setShowCreateButton(true);
-    }, 15000); // 15 секунд
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setShowCreateButton(true);
+        }, 1500);
+        return () => clearTimeout(timer);
+    }, []);
 
-    return () => clearTimeout(timer);
-  }, []);
+    return (
+        <div className={styles.wrapper}>
+            {/* Угловые квадраты */}
+            <div className={`${styles.corner} ${styles.topLeft}`}></div>
+            <div className={`${styles.corner} ${styles.topRight}`}></div>
+            <div className={`${styles.corner} ${styles.bottomLeft}`}></div>
+            <div className={`${styles.corner} ${styles.bottomRight}`}></div>
 
-  return (
-    <div className={styles.container}>
-      <h1 className={styles.title}>Namecodes</h1>
-      <LoadingGrid />
-      {showCreateButton && (
-        <button className={styles.createButton} onClick={() => alert('Комната создана!')}>
-          Создать комнату
-        </button>
-      )}
-    </div>
-  );
+            {/* Основной контент */}
+            <div className={styles.container}>
+                <h1 className={styles.title}>Namecodes</h1>
+                <div className={styles.cube}>
+                    <LoadingGrid/>
+                </div>
+                {/*{showCreateButton && (*/}
+                {/*    <button className={styles.createButton}>*/}
+                {/*        Создать комнату*/}
+                {/*    </button>*/}
+                {/*)}*/}
+            </div>
+        </div>
+    );
 };
 
 export default Home;
